13bca4c35cd029f9c7eb89279e6d0840bac3afb7
ffr96 <frosetti@esquemas.com>
2023-05-31 17:48:25 -0300

    Add schema & config.json for DW1-8

--- main.js
+++ main.js
@@ -1,25 +1,9 @@
 import * as fs from 'fs';
 let settingsCounter = 0;
 
-const buildOption = (chunk) => {
-  let options = [];
-  let obj = {};
-  console.log(chunk);
-  chunk.forEach((option, i) => {
-    if (!isProperty(chunk[i])) {
-      let propIndex = option.indexOf('|');
-      let i18nValue = option.slice(0, propIndex);
-      console.log(i18nValue);
-      let configValue = option.slice(propIndex + 1, option.length);
-      obj['label'] = i18nValue;
-      obj['value'] = configValue;
-      options.push(obj);
-      obj = {};
-    };
-  });
-  return options;
-}
-
+/**
+ * Basic utility
+ */
 const isProperty = (line) => {
   return (
     line.startsWith('id:') ||
@@ -35,23 +19,61 @@ const isSelectProperty = (line) => {
   return (line.includes('|'));
 }
 
+const announcer = (type, ...msg) => {
+  const warning = (text) => `\x1b[33m ${text} \x1b[0m`;
+  const success = (text) => `\x1b[32m ${text} \x1b[0m`;
+  const error = (text) => `\x1b[31m ${text} \x1b[0m`;
+  
+  switch(type) {
+    case 'err':
+      error(msg);
+      return;
+    case 'success':
+      console.log(success(msg));
+      return;
+    case 'warning':
+      console.log(warning(msg));
+      return;
+    default:
+      console.log(msg);
+      return;
+  };
+}
+
+const buildOption = (chunk) => {
+  let options = [];
+  let obj = {};
+  chunk.forEach((option, i) => {
+    if (!isProperty(chunk[i])) {
+      let propIndex = option.indexOf('|');
+      let i18nValue = option.slice(0, propIndex);
+      let configValue = option.slice(propIndex + 1, option.length);
+      obj['label'] = i18nValue;
+      obj['value'] = configValue;
+      options.push(obj);
+      obj = {};
+    };
+  });
+  return options;
+}
+
 const chunkBuilder = (lines) => {
   let chunks = [];
   let chunk = [];
-  
   for (let i = 0; i < lines.length; i++) {
     if (isProperty(lines[i]) || isSelectProperty(lines[i]) || chunk.length === 0) {
       chunk.push(lines[i]);
     };
 
     if (i+1 === lines.length) break;
-    
+
     if (!(isSelectProperty(lines[i+1]) || isProperty(lines[i+1]))) {
       chunks.push(chunk);
       chunk = [];
       chunk.push(lines[i+1]);
     }
   }
+  chunks.push(chunk);
   return chunks;
 }
 
@@ -62,6 +84,7 @@ const buildSetting = (chunk) => {
   chunk.forEach((line, i) => {
     const propIndex = line.indexOf(':');
     let value = null;
+    console.log(line);
     if (propIndex !== -1) {
       const prop = line.slice(0, propIndex);
       value = line.slice(propIndex + 2, line.length);
@@ -69,7 +92,8 @@ const buildSetting = (chunk) => {
     }
 
     if (value === 'select') {
-      obj['option'] = buildOption(chunk.splice(i+1));
+      const mirror = [...chunk];
+      obj['option'] = buildOption(mirror.splice(i+1));
     }
   });
 
@@ -107,10 +131,10 @@ const main = () => {
     const settingsChunk = chunkBuilder(lines);
     const [settings, errs] = settingsBuilder(settingsChunk);
     fs.writeFile('schema.json', JSON.stringify(settings, null, 2), (err) => {
-      if (err) console.error('Error while writing data', err);
+      if (err) return announcer('err', 'Error while writing data');
+      return announcer('success', 'Schema.json completed. Wrote ', settingsCounter, ' settings');
     });
   });
-
 };
 
 main();
